f1becdc67cd931a918172e70dcf324fbed355152
jhin1m <ducanhfake@gmail.com>
2025-07-05 11:02:39 +0700

    feat: Đăng ký TelescopeServiceProvider cho môi trường local
    - Thêm điều kiện để đăng ký TelescopeServiceProvider chỉ khi ứng dụng đang chạy trong môi trường local.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\App\Providers\TelescopeServiceProvider::class);
+        }
     }
 
     /**
