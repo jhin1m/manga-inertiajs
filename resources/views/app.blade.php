8595784e0061735dee6cb0ceea328a02085ccab6
jhin1m <ducanhfake@gmail.com>
2025-07-19 18:33:46 +0700

    feat: add Google Analytics integration for tracking

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,17 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=nunito:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Google Analytics 4 -->
+        @if(config('services.google_analytics.id'))
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google_analytics.id') }}"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '{{ config('services.google_analytics.id') }}');
+        </script>
+        @endif
+
         <!-- Scripts -->
         @routes
         @viteReactRefresh
